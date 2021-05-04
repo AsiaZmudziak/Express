d292fa0194f4ac1be6f16d069dd686479facf240
unknown <asiazmudziak@gmail.com>
2021-05-04 09:49:47 +0200

    add new endpoint

--- server.js
+++ server.js
@@ -7,21 +7,14 @@ const app = express();
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
 });
 
 app.get('/', (req, res) => {
-  res.render('index');;
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
@@ -32,17 +25,21 @@ app.get('/contact', (req, res) => {
   res.render('contact');
 });
 
+app.post('/contact/send-message', (req, res) => {
+  res.json(req.body);
+});
+
 app.get('/info', (req, res) => {
   res.render('info');
 });
 
-app.get('/history', (req, res, next) => {
-  res.render('history');;
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
-})
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
